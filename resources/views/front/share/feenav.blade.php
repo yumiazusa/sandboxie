bc5306db2bc0b3deaf4adee746589d81fe45655e
yumiazusa <yumiazusa@hotmail.com>
2021-04-13 23:29:02 +0800

    salepurchase

--- resources/views/front/share/feenav.blade.php
+++ resources/views/front/share/feenav.blade.php
@@ -32,9 +32,9 @@
     @endif
      <button type="button" class="layui-btn layui-btn-sm  layui-btn-normal" id="bu5" title="重置" url="{{url('renav')}}"><i class="layui-icon">&#xe669;</i></button>
      <button type="button" class="layui-btn layui-btn-sm  layui-btn-normal" id="bu6" title="返回首页"><i class="layui-icon">&#xe68e;</i></button>
-     @if ($user->studentid === "20201013")
+     <!-- @if ($user->studentid === "20201013")
      <button type="button" class="layui-btn layui-btn-sm  layui-btn-normal" id="bu7" title="刷新卡片" url="{{url('refee')}}"><i class="layui-icon">&#xe68e;</i></button>
-     @endif
+     @endif -->
 <table class="layui-table table" id="sharetb" sharetb="{{$sharetb['sharetb']}}">
   <tbody>
     <tr class="tr">
