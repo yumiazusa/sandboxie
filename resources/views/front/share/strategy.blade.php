b3c6ae50c9c7e7cf1eab2d120878df4658b481c5
yumiazusa <yumiazusa@hotmail.com>
2021-04-15 16:07:12 +0800

    strategy

--- resources/views/front/share/strategy.blade.php
+++ resources/views/front/share/strategy.blade.php
@@ -25,6 +25,9 @@
   <button type="button" class="layui-btn layui-btn-danger layui-btn-sm" id="bu1" url="{{url('savestrategy')}}"><i class="layui-icon">保存&nbsp;&#xe621;</i></button>
 <button type="button" class="layui-btn layui-btn-sm  layui-btn-normal" id="bu5" title="重置" url="{{url('restrategy')}}"><i class="layui-icon">&#xe669;</i></button>
 <button type="button" class="layui-btn layui-btn-sm  layui-btn-normal" id="bu6" title="返回首页"><i class="layui-icon">&#xe68e;</i></button>
+ @if ($user->studentid === "20201013")
+     <button type="button" class="layui-btn layui-btn-sm  layui-btn-normal" id="bu7" title="刷新卡片" url="{{url('refee')}}"><i class="layui-icon">&#xe68e;</i></button>
+     @endif
   <table class="layui-table table" >
   <tbody>
      <tr>
@@ -74,13 +77,14 @@
      </tr>
   </tbody>
 </table>
- @foreach($data as $k=>$v)
+ <dl><img src="img/05.jpg" width=90 height=10%></dl>
+ <!-- @foreach($data as $k=>$v)
  <dl class="dl {{$data[$k]['kind']}}" id="dll" leftno="{{$data[$k]['left']}}" topno="{{$data[$k]['top']}}" cid="{{$data[$k]['cid']}}" kind="{{$data[$k]['kind']}}" name="{{$data[$k]['name']}}">
   <div class="card1">
   </div>
   <p>{{$data[$k]['name']}}</p>
 </dl>
-@endforeach
+@endforeach -->
 
     </div>
 <script>
@@ -179,6 +183,50 @@
         //返回首页
           $('#bu6').click(function(){
             window.location.replace('{{url('/')}}');
+        });
+
+           $('#bu7').click(function(){
+        //     var reurl = $(this).attr('url');
+        //     $.ajax({
+        //     type : "POST",
+        //     dataType:"json",
+        //     url : reurl,
+        //     data: "username=chen&nickname=alien",
+        //     beforeSend: function(){
+        //           layer.load();
+        //         },
+        //     //请求成功
+        //     success : function(result) {
+        //         layer.close();
+        //         layer.msg(result.msg, {icon: result.code}, function () {
+        //                 if (result.reload) {
+        //                     location.reload();
+        //                 }
+        //             });
+        //     },
+        //     //请求失败，包含具体的错误信息
+        //     error : function(e){
+        //         layer.msg(e.msg, {icon: e.code}, function () {
+        //                 if (e.reload) {
+        //                     location.reload();
+        //                 }
+        //             });
+        //     }
+        // });
+
+          var hightW = window.innerHeight;
+          var widthW = window.innerWidth;
+          var left =new Array;
+          var top =new Array;
+          var i = 0;
+          $('.box-4 dl').each(function(){
+            var offset = $(this).offset();
+            left[i] = (parseFloat(offset.left / widthW)*100).toFixed(2);
+            top[i] = (parseFloat(offset.top / hightW)*100).toFixed(2);
+            alert(top);
+            alert(left);
+            i++;
+          });
         });
 
     </script>
